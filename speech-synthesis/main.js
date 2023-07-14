ba36251ff60cf8dafe71e12dd94837cb69427c56
Geoff Jamieson <49350011+UnionPAC@users.noreply.github.com>
2023-07-14 13:52:08 -0400

    finished voice-bot project

--- speech-synthesis/main.js
+++ speech-synthesis/main.js
@@ -4,3 +4,44 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
+// message in textarea
+msg.text = document.querySelector('[name="text"]').value;
+
+// get voices
+function populateVoices() {
+  // Returns a list of SpeechSynthesisVoice objects representing all the available voices on the current device.
+  voices = speechSynthesis.getVoices();
+  // put voice options in dropdown list
+  voicesDropdown.innerHTML = voices
+    .filter((voice) => voice.lang.includes("en"))
+    .map(
+      (voice) =>
+        `<option value="${voice.name}">${voice.name} ${voice.lang}</option>`
+    )
+
+    .join("");
+}
+
+// set the voice
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+}
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  // console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice);
+options.forEach((option) => option.addEventListener("change", setOption));
+speakButton.addEventListener("click", toggle);
+stopButton.addEventListener("click", () => toggle(false));
